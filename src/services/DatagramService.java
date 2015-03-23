98b296df82e147cda9ff47aae7440aa7c9663802
xunrongl <lixunrong@gmail.com>
2015-03-23 14:28:29 -0400

    add testing for delay and duplicate

--- src/services/DatagramService.java
+++ src/services/DatagramService.java
@@ -17,6 +17,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Random;
 
 import datatypes.Datagram;
 
@@ -47,7 +48,21 @@ public class DatagramService {
 		DatagramPacket packet = new DatagramPacket(data, data.length,
 				IPAddress, datagram.getDstport());
 
-		// Send packet
+        Random numGenerator = new Random();
+        int num = numGenerator.nextInt(10);
+
+        // Testing for delay packet
+        if (num == 3){
+            int randomNum = numGenerator.nextInt(10);
+            delayPacket(packet, randomNum * 1000);
+            System.out.println("Testing for delay packet, delaying" + randomNum + "seconds");
+        }
+		// Testing for duplicate packet
+        if (num == 7) {
+            int randomNum = numGenerator.nextInt(10);
+            duplicatePacket(packet, randomNum);
+            System.out.println("Testing for duplicate packet, duplicating" + randomNum + "times");
+        }
 		socket.send(packet);
 	}
 
@@ -66,4 +81,24 @@ public class DatagramService {
 
 		return datagram;
 	}
+
+    protected void delayPacket(DatagramPacket packet, int delay){
+        try {
+            Thread.sleep(delay);
+            socket.send(packet);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void duplicatePacket(DatagramPacket packet, int duplicate){
+        for (int i = 0; i < duplicate; i++)
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
 }
